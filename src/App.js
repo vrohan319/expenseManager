e253683d712466ed291f0b4750daeff03246a9f8
Aman Kumar <vrohan319@gmail.com>
2022-07-21 20:49:37 +0530

    reusing react components with hard coded data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
