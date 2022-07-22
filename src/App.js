f72fe2d584f30bbeb7b81cf22694f7014a0c0114
Aman Kumar <vrohan319@gmail.com>
2022-07-22 11:39:30 +0530

    passed data to expense component successfully

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import Expenses from "./components/Expenses";
 
 function App() {
 
+  const expenses = [
+    {
+      id: "e1",
+      title: "ToiletPaper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses />
+      <Expenses items={expenses}/>
     </div>
   );
 }
