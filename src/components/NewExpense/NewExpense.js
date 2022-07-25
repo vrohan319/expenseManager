6e08feb0d60b143a436a72b35cf7e08bcb8a6689
Aman Kumar <vrohan319@gmail.com>
2022-07-25 14:20:44 +0530

    added filter by copying the code and passed data child to parent

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,19 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css"
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+    const saveExpeneseDataHander = (enteredExpensesData) => {
+        const expenseData = {
+            ...enteredExpensesData,
+            id : Math.random().toString() 
+        } ;
+        props.onAddExpense(expenseData) ;
+    } ;
+
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpeneseData = {saveExpeneseDataHander} />
         </div>
     ) ;
 } ;
